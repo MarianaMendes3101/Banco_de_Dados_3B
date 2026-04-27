b73fe55d253b1cd4886735366a736e2775053d67
MarianaMendes3101 <mendesmariana3101@gmail.com>
2026-04-27 15:19:31 -0400

    criação da classe de conexão

--- class/Database.class.php
+++ class/Database.class.php
@@ -1,1 +1,33 @@
 <?php
+final class Database{
+    private static $instance = null;
+    private $connection;
+
+    private function _construct(){
+        try {
+            $config = parse_ini_file(__DIR__."/../config.ini", true)["database"];
+            $dns="";
+            if($config["driver"]=== "mysql"){
+                $dns= "mysql:host={$config['host']};port={$config['port']};dbname={$config['data']};charset=uft8";
+            } elseif ($config["driver"]=== "pgsql"){
+                $dns= "pgsql:host={$config['host']};port={$config['port']};dbname={$config['data']}";
+            }else{
+                throw new Exception("Drive de banco de dados não suportado".$config['driver']);
+            }
+            $this->connection = new PDO($dns,$config['user'], $config['pass']);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+           error_log('Erro de conexão' .$e->getMessage());
+           die('Erro ao conectar com o banco de dados. Tente mais tarde');
+        }
+    }
+    public static function getInstance(){
+        if(self::$instance===null){ 
+            self::$instance = new Database();
+    }
+    return self::$instance;
+    } 
+    public function getConnection(){
+        return $this->connection;               
+    }
+}
