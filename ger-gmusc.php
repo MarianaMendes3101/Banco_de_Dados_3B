7d546768929497d1c8c14ad0552b21cc6dbf0455
MarianaMendes3101 <mendesmariana3101@gmail.com>
2026-04-28 08:48:24 -0400

    Aula 28 do 04 criação do ger-gm

--- ger-gmusc.php
+++ ger-gmusc.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
-<html lang="pt_br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" >
 
-    <title>Dashboard | Personal</title>
+
+    <title>Exercício | PersonalPRO</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">PersonalPro</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Grupo Muscular</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Exercício</a>
-        </li>      
-          </ul>
-        </li>
-      </ul>
-    </div>
+<?php require_once "_parts/_menu.php" ?>
+<main class="container" style="margin-top: 80px;">
+  <div class="mt-5">
+    <h4>Cadastro de grupo muscular</h4>
   </div>
-</nav>
-<main class="container"></main>
+  <div class="card">
+    <form action="" method="post" class="row p-4 g3 mt-3">
+      <div class="cool-12">
+        <label for="nome" class="form-label">Nome</label>
+        <input type="text" name="nome" id="nome" class="form-control">
+
+
+      </div>
+      <div class="mt-3">
+        <a href="gmuscular.php" class="btn bnt-secondary">Cancelar</a>
+        <button type="submit" class="btn bnt-success">Salvar</button>
+      </div>
+    </form>
+  </div>
+   
+</main>
+
+
 
 
 
--- gmuscular.php
+++ gmuscular.php
@@ -1,62 +1,50 @@
 <!DOCTYPE html>
-<html lang="pt_br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <title> Grupo Musculares | Personal</title>
+
+
+    <title>Grupo Muscular | PersonalPRO</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">PersonalPro</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  active" aria-current="page"  href="#">Grupo Muscular</a>
-
-        <li class="nav-item">
-          <a class="nav-link  active" aria-current="page"  href="#">Exercício</a>
-        </li>      
-          </ul>
-        </li>
-      </ul>
+<?php require_once "_parts/_menu.php" ?>
+<main class="container">
+    <div class="mt-5 d-flex justify-content-between p-5">
+      <h3>Grupos Musculares</h3>
+      <a href="ger-gmusc.php"class="btn btn-success">Novo Grupo Muscular</a>
     </div>
-  </div>
-</nav>
-<main class="container"></main>
-  <div class="mt-5 d-flex justify-content-between p-5">
-    <h3>Grupos Muscular</h3>
-    <a href="ger-muscuar.php" class= "btn btn-primary"> Novo Grupo Muscular</a>
 
-  </div>
 
-  <table class="table p-3">
-    <thead>
-      <tr>        
-        <th>Nome</th>]
-        <th class="text-center">Ações</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-    <td>Ombro</td>
-    <td class="text-center">
-          <a href="#" class="btn btn-outline-info btn-sm"><i class="bi bi-eye"></i></a>
-          <a href="#" class="btn btn-outline-primary btn-sm"><i class="bi bi-pencil-square"></i></a>
-          <a href="#" class="btn btn-outline-danger btn-sm"><i class="bi bi-trash"></i></a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    <table class="table p-1">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th class="text-center">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Ombro</td>
+          <td class="text-center">
+            <a href="#"class="btn btn-outline-info btn-sm">
+              <i class="bi bi-eye"></i>
+            </a>
+            <a href="#"class="btn btn-outline-primary btn-sm">
+              <i class="bi bi-pencil-square"></i>
+            </a>
+            <a href="#"class="btn btn-outline-danger btn-sm">
+              <i class="bi bi-trash"></i>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
+
+</main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" ></script>
 </body>
